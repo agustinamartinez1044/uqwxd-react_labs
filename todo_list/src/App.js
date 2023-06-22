01f20caad0f1ea78785c9c8b3399b88193940691
agustinamartinez1044 <agustina.martinez@tiendamia.com>
2023-06-22 00:50:37 -0300

    Create and Implement code to add task

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -3,27 +3,38 @@ import "./App.css";
 const App = () => {
   const [todos, setTodos] = React.useState([]);
   const [todo, setTodo] = React.useState("");
-  
-  // Add the handlesubmit code here
-  
-  
-  // Add the deleteToDo code here
-
-  
-  // Add the toggleComplete code here
-
-  
-  // Add the submitEdits code here
-
-  
-return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
-<button type ="submit">Add Todo</button>
-</form>
-</div>
-);
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+    if (newTodo.text.length > 0) {
+      setTodos([...todos].concat(newTodo));
+      setTodo("");
+    } else {
+      alert("Enter Valid Task");
+      setTodo("");
+    }
+  }
+  return (
+    <div id="todo-list">
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      {todos.map((todo) => (
+        <div className="todo" key={todo.id}>
+          <div>{todo.text}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 export default App;
